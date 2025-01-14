6a9226786737127f89fe54e06ad852d49dbc0fa3
Jayden <jmforde@deakin.edu.au>
2025-01-14 19:41:21 +1100

    adding /updateUserPassword API endpoint and logic.

--- grocify/backend/server.js
+++ grocify/backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
@@ -7,6 +8,15 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Enable CORS
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:4000'],
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+
+app.use(cors(corsOptions));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
@@ -23,7 +33,7 @@ app.use('/api/user', userRoutes);
 
 // Default route
 app.get('/', (req, res) => {
-  res.send('Welcome to the User Registration and Login API!');
+  res.send('Welcome to Grocify Backend Server!');
 });
 
 // Start the server
