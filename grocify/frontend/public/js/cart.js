c6ce6cfc21e85282d76c8ee376ad157f9d7baac5
sonny-repo <s224873117@deakin.edu.au>
2025-02-05 07:37:05 +0800

    clean up the unused code

--- grocify/frontend/public/js/cart.js
+++ grocify/frontend/public/js/cart.js
@@ -11,10 +11,7 @@ function updateCart() {
 
   cartContainer.innerHTML = cart.map(item => `
       <div class="cart-item">
-          ${item.image}
             <div>${item.name}</div> 
-            <div>$${item.price.toFixed(2)}</div>
-            <div>(x${item.quantity})</div>
             <a href="${item.buyURL}" target="_blank" class="btn-small blue">Buy Now</a>
             <i class="material-icons" onclick="removeFromCart(${item.id})">close</i>
       </div>
