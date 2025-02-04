ae64e335a9c12e07fe495ce02df932ffae2d0559
sonny-repo <s224873117@deakin.edu.au>
2025-02-05 05:25:13 +0800

    update script

--- grocify/frontend/public/js/scripts.js
+++ grocify/frontend/public/js/scripts.js
@@ -23,3 +23,20 @@
         alert('Please enter a search term.');
         }
     });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener("click", function (e) {
+                e.preventDefault();
+                const targetId = this.getAttribute("href").substring(1);
+                const targetElement = document.getElementById(targetId);
+                
+                if (targetElement) {
+                    window.scrollTo({
+                        top: targetElement.offsetTop - 70, // Adjust offset if needed
+                        behavior: "smooth"
+                    });
+                }
+            });
+        });
+    });
